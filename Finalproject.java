97ce95ec4905b6e5a90a9703f0a947faefdfb028
sailorjim <lana.dayus@gmail.com>
2020-04-28 13:50:01 +1000

    dsdasdsadasdasd

---
+++ Finalproject.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalproject;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+/**
+ *
+ * @author Thopte
+ */
+public class Finalproject {
+    static String input;
+    static Scanner in = new Scanner(System.in);
+    static ArrayList<String> inventory= new ArrayList<>();
+    
+public static void main(String[] args) {
+    game newgame = new game();
+    newgame.createNewGame();
+    
+
+
+}
+   
+}
+
+
+
---
+++ chinbu.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalproject;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Thopte
+ */
+public class chinbu {
+         static location currentLocation;
+        static final  ArrayList<item> inventory= new ArrayList<>();
+        
+      
+         static void setLocation(location location){
+             currentLocation=location;
+         }
+         static void takeItem(String checkItem){
+             boolean itemHere = currentLocation.checkitem(checkItem);
+             if(itemHere){
+                 item currentLocation.
+                 inventory.add(areaItem);
+                 currentLocation.removeItem(areaItem);
+                 game.playerInput();
+           } else {
+                    System.out.println("Item is not here!");
+                    game.playerInput();
+                   }
+     
+         };
+         static location getLocation(){
+             return currentLocation;
+         }
+         static void openInventory(){
+             for (item e:inventory){
+                 System.out.println (e.getName()+ ",");
+                 game.playerInput();
+             }
+         }
+    }
